655218ecca23695367309911c923e2bc21213664
Michael Hernandez <gammabeta010@gmail.com>
2020-04-07 23:43:25 +0200

    Finito Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+    /*
     var json = [
         {type:"lightblue",      amount:15},
         {type:"lightcoral",     amount:5},
@@ -8,6 +10,44 @@ $(document).ready(function () {
         {type:"lightblue",      amount:25},
         {type:"lightblue",      amount:5}
     ]
+    */
+
+    var json = [];
+
+    $.ajax({
+        url: 'http://157.230.17.132:4012/sales',
+        method: 'GET',
+        async: false,
+        success: function(data) {
+            //console.log(data);
+            //console.log(data.length);
+
+            for (var i = 0; i < data.length; i++) {
+                //console.log(data[i]);
+                var salesData = {
+                    id: data[i].id,
+                    salesman: data[i].salesman,
+                    amount: data[i].amount,
+                    date: data[i].date,
+                    month: moment(data[i].date, 'DD-MM-YYYY').locale('it').format('M')
+                }
+                //console.log(salesData);
+                json.push(salesData);
+            }
+            console.log(json);
+
+            console.log(json[0].month - json[1].month);
+        }
+
+    });
+
+    json.sort(function(a,b) {
+        return a.month - b.month
+    })
+
+    console.log(json);
+
+
 
     // ------------------- VERSIONE A MANO -------------------
 
@@ -29,40 +69,64 @@ $(document).ready(function () {
 
     for (var i = 0; i < json.length; i++) {
         var oggettoSingolo = json[i];
-        // console.log(oggettoSingolo);
-        // console.log(oggettoSingolo.type);
-        // console.log(oggettoSingolo.amount);
-        var colore = oggettoSingolo.type;
-        if (oggettoIntermedio[colore] === undefined) {      // nel caso in cui la chiave non esiste
-            oggettoIntermedio[colore] = 0;                  //  allora la creiamo e gli assegnamo il valore 0
+        var currDate = moment(oggettoSingolo.date, 'DD-MM-YYYY').locale('it');
+
+         //console.log(oggettoSingolo);
+         //console.log(oggettoSingolo.date);
+         //console.log(currDate.format('MM'));
+         //console.log(oggettoSingolo.amount);
+        var mese = currDate.format('MMMM');
+        if (oggettoIntermedio[mese] === undefined) {      // nel caso in cui la chiave non esiste
+            oggettoIntermedio[mese] = 0;                  //  allora la creiamo e gli assegnamo il valore 0
         }
         // console.log(oggettoIntermedio);
-        oggettoIntermedio[colore] += oggettoSingolo.amount;     // (oramai la chiave esiste) e al suo valore sommiamo quello dell'amount dell'iesimo oggetto singolo che stiamo ciclando
+        oggettoIntermedio[mese] += oggettoSingolo.amount;     // (oramai la chiave esiste) e al suo valore sommiamo quello dell'amount dell'iesimo oggetto singolo che stiamo ciclando
     }
-    // console.log(oggettoIntermedio);
+    console.log(oggettoIntermedio);
 
-    var labelsPC = [];
-    var dataPC = [];
+    var labelsLine = [];
+    var dataLine = [];
 
     for (var key in oggettoIntermedio) {    // ciclo nell'oggettoIntermedio per prendermi le chiavi e trasformarle in 'labels' e i valori (di quella chiave) per trasformarli in 'data'
         // console.log(key);
-        labelsPC.push(key);
-        dataPC.push(oggettoIntermedio[key]);
+        labelsLine.push(key);
+        dataLine.push(oggettoIntermedio[key]);
     }
-    // console.log(labelsPC);
-    // console.log(dataPC);
+     //console.log(labelsLine);
+     //console.log(dataLine);
 
     var ctx = $('#grafico');
     var chart = new Chart(ctx, {
 
-        type: 'pie',
+        type: 'line',
         data: {
+            labels: labelsLine,
             datasets: [{
-                data: dataPC,
-                backgroundColor: labelsPC
+                backgroundColor: 'blue',
+                pointStyle: 'square',
+                borderColor: 'blue',
+                fill: false,
+                lineTension: 0,
+                showLine: true,
+                label: '2017',
+                data: dataLine,
             }],
 
-            labels: labelsPC
+
+        },
+        options: {
+            elements: {
+                point: {
+                    radius: 0
+                }
+            },
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        drawBorder: false,
+                    },
+                }]
+            }
         }
     });
 
