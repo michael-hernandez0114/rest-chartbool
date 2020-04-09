f4b7d9a21aae599ab440054156fb0b82441e580c
Michael Hernandez <gammabeta010@gmail.com>
2020-04-09 11:03:49 +0200

    WIP Milestone 2 (not finished)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
-    var json = [];
+    var apiBaseURL = 'http://157.230.17.132:4012/sales';
+
 
     $.ajax({
-        url: 'http://157.230.17.132:4012/sales',
+        url: apiBaseURL,
         method: 'GET',
         //async: false,
         success: function(data) {
@@ -26,77 +27,17 @@ $(document).ready(function () {
 
     });
 
+    $('#btn-submit').on('click', function(){
+        var venditoreSelezionato = $('#venditore').val();
+        var dateSelected = $('#sales-date').val();
+        var formattedDate = moment(dateSelected).format('DD/MM/YYYY');
 
+        console.log(venditoreSelezionato);
+        console.log(dateSelected);
+        console.log(formattedDate);
 
-    //console.log(json);
-
-    // Ora i passaggi a mano dobbiamo farli fare al computer
-/*
-    var oggettoIntermedio = {};     // creo un oggetto vuoto dove inseriremo i valori dei 'json.type' come chiavi e come valori le somme degli 'amount'
-
-    for (var i = 0; i < json.length; i++) {
-        var oggettoSingolo = json[i];
-        var currDate = moment(oggettoSingolo.date, 'DD-MM-YYYY').locale('it');
-
-         //console.log(oggettoSingolo);
-         //console.log(oggettoSingolo.date);
-         //console.log(currDate.format('MM'));
-         //console.log(oggettoSingolo.amount);
-        var mese = currDate.format('MMMM');
-        if (oggettoIntermedio[mese] === undefined) {      // nel caso in cui la chiave non esiste
-            oggettoIntermedio[mese] = 0;                  //  allora la creiamo e gli assegnamo il valore 0
-        }
-        // console.log(oggettoIntermedio);
-        oggettoIntermedio[mese] += oggettoSingolo.amount;     // (oramai la chiave esiste) e al suo valore sommiamo quello dell'amount dell'iesimo oggetto singolo che stiamo ciclando
-    }
-    console.log(oggettoIntermedio);
-
-    var labelsLine = [];
-    var dataLine = [];
-
-    for (var key in oggettoIntermedio) {    // ciclo nell'oggettoIntermedio per prendermi le chiavi e trasformarle in 'labels' e i valori (di quella chiave) per trasformarli in 'data'
-        // console.log(key);
-        labelsLine.push(key);
-        dataLine.push(oggettoIntermedio[key]);
-    }
-     //console.log(labelsLine);
-     //console.log(dataLine);
-
-    var ctx = $('#grafico');
-    var chart = new Chart(ctx, {
-
-        type: 'line',
-        data: {
-            labels: labelsLine,
-            datasets: [{
-                backgroundColor: 'blue',
-                pointStyle: 'square',
-                borderColor: 'blue',
-                fill: false,
-                lineTension: 0,
-                showLine: true,
-                label: '2017',
-                data: dataLine,
-            }],
-
-
-        },
-        options: {
-            elements: {
-                point: {
-                    radius: 0
-                }
-            },
-            scales: {
-                yAxes: [{
-                    gridLines: {
-                        drawBorder: false,
-                    },
-                }]
-            }
-        }
     });
-*/
+
     function processSalesPerMonth(salesData){
         var salesArray = [];
         var finalObject = {};
@@ -208,6 +149,13 @@ $(document).ready(function () {
 
     }
 
+    function pushSalesData(newData){
+
+        $.ajax({
+
+        })
+    }
+
     function createLineChart(chartData) {
         var ctx = $('#linechart');
         var chart = new Chart(ctx, {
@@ -264,7 +212,4 @@ $(document).ready(function () {
             }
         });
     }
-
-
-
 });
