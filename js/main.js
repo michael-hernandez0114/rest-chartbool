4bb45107b982bda1a6683f0b82c19db7916158b1
Michael Hernandez <gammabeta010@gmail.com>
2020-04-11 12:54:46 +0200

    Finito Milestone 2 e 3. Pulito il codice con funzione.

--- js/main.js
+++ js/main.js
@@ -2,100 +2,69 @@ $(document).ready(function () {
 
     var apiBaseURL = 'http://157.230.17.132:4012/sales';
 
-
-    $.ajax({
-        url: apiBaseURL,
-        method: 'GET',
-        //async: false,
-        success: function(data) {
-            //console.log(data);
-            //console.log(data.length);
-
-            var dataCopy = data;
-            var salesObject = processSalesPerMonth(dataCopy);
-            //console.log(salesObject);
-            createLineChart(salesObject);
-            var venditeData = processContributoVenditore(dataCopy);
-            createTorta(venditeData);
-
-
-
-            //console.log(json);
-
-            //console.log(json[0].month - json[1].month);
-        }
-
-    });
+    drawCharts();
 
     $('#btn-submit').on('click', function(){
-        var venditoreSelezionato = $('#venditore').val();
         var dateSelected = $('#sales-date').val();
-        var formattedDate = moment(dateSelected, 'YYYY-MM-DD').format('DD/MM/YYYY');
-        var salesAmount = $('#sales').val();
+        var newSalesEntry = {
+            salesman: $('#venditore').val(),
+            date: moment(dateSelected, 'YYYY-MM-DD').format('DD/MM/YYYY'),
+            amount:parseInt($('#sales').val())
+        };
 
-        $.ajax({
-            url: apiBaseURL,
-            method: 'POST',
-            data: {
-                salesman: venditoreSelezionato,
-                date: dateSelected,
-                sales: salesAmount
-            },
-            success: function(data, stato) {
-
-            },
-            error: function(error){
-                alert('the PUSH API has errored:' + error)
-            }
-        })
-
-        console.log(venditoreSelezionato);
-        console.log(dateSelected);
-        console.log(formattedDate);
-        console.log(salesAmount);
+        pushSalesData(newSalesEntry);
+        drawCharts();
 
     });
 
-    function processSalesPerMonth(salesData){
-        var salesArray = [];
+    function drawCharts() {
+
+        $('#linechart').empty();
+        $('#torta').empty();
+
+            $.ajax({
+                url: apiBaseURL,
+                method: 'GET',
+                success: function(data) {
+                    //console.log(data);
+                    //console.log(data.length);
+
+                    var dataCopy = data;
+                    var salesArrayObjs = collectAPIData(dataCopy);
+                    var salesObject = processSalesPerMonth(salesArrayObjs);
+                    //console.log(salesObject);
+                    createLineChart(salesObject);
+                    var venditeData = processContributoVenditore(salesArrayObjs);
+                    createTorta(venditeData);
+                    var salesQuarterObject = processSalesPerQuarter(salesArrayObjs);
+                    createBarChart(salesQuarterObject);
+
+                }
+
+            })
+    }
+
+    function processSalesPerMonth(salesArray){
         var finalObject = {};
-
-        for (var i = 0; i < salesData.length; i++) {
-            //console.log(data[i]);
-            var salesEntry = {
-                id: salesData[i].id,
-                salesman: salesData[i].salesman,
-                amount: salesData[i].amount,
-                date: salesData[i].date,
-                month: moment(salesData[i].date, 'DD-MM-YYYY').locale('it').format('M')
-            }
-            //console.log(salesData);
-            salesArray.push(salesEntry);
-        }
-
-        salesArray.sort(function(a,b) {
-            return a.month - b.month
-        })
-
-        var oggettoIntermedio = {};     // creo un oggetto vuoto dove inseriremo i valori dei 'json.type' come chiavi e come valori le somme degli 'amount'
+        var oggettoIntermedio = {};
 
         for (var i = 0; i < salesArray.length; i++) {
             var oggettoSingolo = salesArray[i];
             var currDate = moment(oggettoSingolo.date, 'DD-MM-YYYY').locale('it');
 
             var mese = currDate.format('MMMM');
-            if (oggettoIntermedio[mese] === undefined) {      // nel caso in cui la chiave non esiste
-                oggettoIntermedio[mese] = 0;                  //  allora la creiamo e gli assegnamo il valore 0
+            if (oggettoIntermedio[mese] === undefined) {
+                oggettoIntermedio[mese] = 0;
             }
             // console.log(oggettoIntermedio);
-            oggettoIntermedio[mese] += oggettoSingolo.amount;     // (oramai la chiave esiste) e al suo valore sommiamo quello dell'amount dell'iesimo oggetto singolo che stiamo ciclando
+            oggettoIntermedio[mese] += oggettoSingolo.amount;
         }
         //console.log(oggettoIntermedio);
 
         var labelsLine = [];
         var dataLine = [];
 
-        for (var key in oggettoIntermedio) {    // ciclo nell'oggettoIntermedio per prendermi le chiavi e trasformarle in 'labels' e i valori (di quella chiave) per trasformarli in 'data'
+        for (var key in oggettoIntermedio) {
             // console.log(key);
             labelsLine.push(key);
             dataLine.push(oggettoIntermedio[key]);
@@ -109,50 +78,64 @@ $(document).ready(function () {
         return finalObject;
     }
 
-    function processContributoVenditore(tortaData) {
-        var venditeArray = [];
+    function collectAPIData(input) {
+        var dataArray = [];
+
+        for (var i = 0; i < input.length; i++) {
+            //console.log(data[i]);
+            var salesEntry = {
+                salesman: input[i].salesman,
+                amount: parseInt(input[i].amount),
+                date: input[i].date,
+                month: moment(input[i].date, 'DD-MM-YYYY').locale('it').format('M')
+            }
+            //console.log(salesData);
+            //console.log(input[i].amount);
+            dataArray.push(salesEntry);
+        }
+
+        dataArray.sort(function(a,b) {
+            return a.month - b.month
+        })
+
+        return dataArray;
+    }
+
+    function processContributoVenditore(apiData) {
         var finalObject = {};
         var totalSales = 0;
 
-        for (var i = 0; i < tortaData.length; i++) {
-            //console.log(data[i]);
-            var vendite = {
-                salesman: tortaData[i].salesman,
-                amount: tortaData[i].amount
-            }
-            //console.log(salesData);
-            venditeArray.push(vendite);
-        }
+        var oggettoIntermedio = {};
 
-        var oggettoIntermedio = {};     // creo un oggetto vuoto dove inseriremo i valori dei 'json.type' come chiavi e come valori le somme degli 'amount'
-
-        for (var i = 0; i < venditeArray.length; i++) {
-            var oggettoSingolo = venditeArray[i];
+        for (var i = 0; i < apiData.length; i++) {
+            var oggettoSingolo = apiData[i];
             var salesMan = oggettoSingolo.salesman;
 
-            if (oggettoIntermedio[salesMan] === undefined) {      // nel caso in cui la chiave non esiste
-                oggettoIntermedio[salesMan] = 0;                  //  allora la creiamo e gli assegnamo il valore 0
+            if (oggettoIntermedio[salesMan] === undefined) {
+                oggettoIntermedio[salesMan] = 0;
             }
             // console.log(oggettoIntermedio);
-            oggettoIntermedio[salesMan] += oggettoSingolo.amount;     // (oramai la chiave esiste) e al suo valore sommiamo quello dell'amount dell'iesimo oggetto singolo che stiamo ciclando
+            oggettoIntermedio[salesMan] += oggettoSingolo.amount;
         }
-        console.log(oggettoIntermedio);
+        //console.log(oggettoIntermedio);
 
+        // calculate total sales from individual sales
         for (var amount in oggettoIntermedio) {
-            totalSales += oggettoIntermedio[amount];
+            totalSales += parseInt(oggettoIntermedio[amount]);
         }
 
+        // calculate percentage of sales per venditore using totalSales
         for (var amount in oggettoIntermedio) {
             oggettoIntermedio[amount] = ((oggettoIntermedio[amount] / totalSales) * 100).toFixed(2);
         }
 
-        console.log(totalSales);
-        console.log(oggettoIntermedio);
+        //console.log(totalSales);
+        //console.log(oggettoIntermedio);
 
         var labelsTorta = [];
         var dataTorta = [];
 
-        for (var key in oggettoIntermedio) {    // ciclo nell'oggettoIntermedio per prendermi le chiavi e trasformarle in 'labels' e i valori (di quella chiave) per trasformarli in 'data'
+        for (var key in oggettoIntermedio) {
             // console.log(key);
             labelsTorta.push(key);
             dataTorta.push(oggettoIntermedio[key]);
@@ -161,14 +144,65 @@ $(document).ready(function () {
         finalObject.labels = labelsTorta;
         finalObject.allData = dataTorta;
 
-        console.log(finalObject);
+        //console.log(finalObject);
 
         return finalObject;
 
     }
 
-    function pushSalesData(newData){
+    function processSalesPerQuarter(apiData) {
+        var finalObject = {};
+        var oggettoIntermedio = {};
 
+        for (var i = 0; i < apiData.length; i++) {
+            var oggettoSingolo = apiData[i];
+            var currQuarter = moment(oggettoSingolo.date, 'DD-MM-YYYY').quarter();
+
+            console.log(currQuarter);
+
+            if (oggettoIntermedio[currQuarter] === undefined) {
+                oggettoIntermedio[currQuarter] = 0;
+            }
+            // console.log(oggettoIntermedio);
+            oggettoIntermedio[currQuarter] += oggettoSingolo.amount;
+        }
+        console.log(oggettoIntermedio);
+
+        var labelsLine = [];
+        var dataLine = [];
+
+        for (var key in oggettoIntermedio) {
+            // console.log(key);
+            labelsLine.push(key);
+            dataLine.push(oggettoIntermedio[key]);
+        }
+
+        finalObject.labels = labelsLine;
+        finalObject.allData = dataLine;
+
+        //console.log(finalObject);
+
+        return finalObject;
+    }
+
+    function pushSalesData(newSales){
+
+        $.ajax({
+            url: apiBaseURL,
+            method: 'POST',
+            data: {
+                salesman: newSales.salesman,
+                amount: newSales.amount,
+                date: newSales.date
+
+            },
+            success: function(data, stato) {
+                //console.log(data);
+            },
+            error: function(error){
+                alert('the PUSH API has errored:' + error)
+            }
+        })
 
     }
 
@@ -222,6 +256,25 @@ $(document).ready(function () {
                     borderColor: 'none',
                     borderWidth: 0,
                     data: tortaData.allData,
+                }],
+
+
+            }
+        });
+    }
+
+    function createBarChart(barChartData) {
+        var ctx = $('#bar');
+        var chart = new Chart(ctx, {
+
+            type: 'bar',
+            data: {
+                labels: barChartData.labels,
+                datasets: [{
+                    backgroundColor: ['blue','green','red','yellow'],
+                    borderColor: 'none',
+                    borderWidth: 0,
+                    data: barChartData.allData,
                 }],
 
 
